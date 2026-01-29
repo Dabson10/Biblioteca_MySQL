09508e33f941df1e6e4ee8494b9657c48775a60d
Dabson10 <dabson753@gmail.com>
2026-01-28 22:48:44 -0600

    Se realizo la conexion a base de datos y se reutilizo el diagrama de clases del proyecto 7 del mi repo Ejercicios-POO.

---
+++ src/main/java/Conexion/Conexion.java
@@ -0,0 +1,29 @@
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    Connection conectar ;
+    String usuario = "root";
+    String clave = "Dabson12";
+    String baseD = "estudio_dabson";
+    String ubicacion = "localhost";
+    String puerto = "3306";
+    String url ="jdbc:mysql://" + ubicacion + ":" + puerto + "/" + baseD;
+
+    public void conectarse(){
+
+        try{
+            //La siguiente línea accede a la carpeta external libraries y sigue el orden,
+            //Abre la carpeta "com.mysql", después "cj", después "jdbc" y al final ejecuta la clase "Driver"
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //Dirección formada con los datos de la base de datos y claves.
+            conectar = DriverManager.getConnection(url, usuario, clave);
+            System.out.println("Conexión realizada con éxito.");
+        }catch(Exception e){
+            System.out.println("Problemas con la conexión: " + e.getMessage());
+        }
+    }
+
+}
